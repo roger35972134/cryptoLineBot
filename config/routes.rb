ffdbb64bcdd07d2a3e4ad26b0b6209d40b60530f
roger <roger35972134@gmail.com>
2017-12-13 14:35:11 +0800

    webhook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'search_by_abbreviation'   => 'currency#search_by_abbreviation'
+  post 'webhook'                  => 'currency#webhook'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
