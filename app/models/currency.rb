d689ca7c998122b4daa17c2ad7db2ba06fea5fcf
roger <roger35972134@gmail.com>
2017-12-13 14:29:55 +0800

    change subdomain

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -45,7 +45,7 @@ class Currency < ActiveRecord::Base
 	  }
 	end
 
-	post '/callback' do
+	post '/webhook' do
 	  body = request.body.read
 	  puts body
 
@@ -65,11 +65,6 @@ class Currency < ActiveRecord::Base
 	          text: event.message['text']
 	        }
 	        client.reply_message(event['replyToken'], message)
-	      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-	        response = client.get_message_content(event.message['id'])
-	        tf = Tempfile.open("content")
-	        tf.write(response.body)
-	      end
 	    end
 	  }
 
