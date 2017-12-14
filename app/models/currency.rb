c1e57dddf22950dbee529dc97985129b55fa8856
roger <roger35972134@gmail.com>
2017-12-14 14:52:38 +0800

    change message content

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,51 +2,58 @@ class Currency < ActiveRecord::Base
 
 	require 'open-uri'
 	def self.get_currency
-		books = Nokogiri::HTML(open("https://www.coingecko.com/en"))
-		name = []
-		abbreviation = []
-		rate = []
-		exchange = []
+		begin
+		  	books = Nokogiri::HTML(open("https://www.coingecko.com/en"))
+			name = []
+			abbreviation = []
+			rate = []
+			exchange = []
 
-		books.css('.coin-content-name').each do |link|
-			name.push link.content
+			books.css('.coin-content-name').each do |link|
+				name.push link.content
+			end
+
+			books.css('.coin-crypto-symbol').each do |link|
+				abbreviation.push link.content
+			end
+			
+			books.css('.stat-percent').each do |link|
+				rate.push link.content
+			end
+
+			books.css('.currency-exchangable').each do |link|
+			  	s = link.content.split("\n")[1]
+			  	exchange.push s
+			end
+
+			(0..name.count-1).each do |i|
+				currency = Currency.find_by(:name => name[i])
+				currency = Currency.create(:name => name[i]) if currency.nil?
+
+				currency.abbreviation = abbreviation[i]
+				currency.value = exchange[i*3]
+				currency.rate = rate[i]
+				currency.market_exchange = exchange[i*3+2]
+				currency.save
+			end
+		rescue OpenURI::HTTPError => e
+			return "Server Error"
 		end
 
-		books.css('.coin-crypto-symbol').each do |link|
-			abbreviation.push link.content
-		end
-		
-		books.css('.stat-percent').each do |link|
-			rate.push link.content
-		end
-
-		books.css('.currency-exchangable').each do |link|
-		  	s = link.content.split("\n")[1]
-		  	exchange.push s
-		end
-
-		(0..name.count-1).each do |i|
-			currency = Currency.find_by(:name => name[i])
-			currency = Currency.create(:name => name[i]) if currency.nil?
-
-			currency.abbreviation = abbreviation[i]
-			currency.value = exchange[i*3]
-			currency.rate = rate[i]
-			currency.market_exchange = exchange[i*3+2]
-			currency.save
-		end
+		return "OK"
 	end
 
 	def self.search_by_abbreviation(abbreviation)
-		get_currency
+		if get_currency == "OK"
+			c = Currency.find_by(:abbreviation => abbreviation)
 
-		c = Currency.find_by(:abbreviation => abbreviation)
-
-		unless c.nil?
-			res = "Coin: #{c.name}\nPrice: #{c.value}\nMKT CAP: #{c.market_exchange}"
+			unless c.nil?
+				res = "貨幣: #{c.name}\n價格: #{c.value}\n市場價值: #{c.market_exchange}"
+			else
+				res = "請輸入正確的貨幣代號，例如：BTC (Bitcoin)!"
+			end
 		else
-			res = "Coin not found !"
+			res = "伺服器目前維護中...\n請稍候再試，謝謝您的支持(oops)"
 		end
-		res
 	end
 end
