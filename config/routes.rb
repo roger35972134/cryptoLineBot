89d1c3fbe68201bd9e16be16dd6ab08e5ef80a87
roger <roger35972134@gmail.com>
2017-12-21 16:17:24 +0800

    bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   post 'webhook'                  => 'currency#webhook'
   post 'webhook2'                  => 'currency#webhook2'
   post 'webhook3'                  => 'currency#webhook3'
-  #get 'webhook_facebook'         => 'currency#webhook_facebook'
-  mount Messenger::Bot::Space => "/webhook_facebook"
+  get 'webhook_facebook'         => 'currency#webhook_facebook'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
